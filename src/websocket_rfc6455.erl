0d6716fb5c9e951b900c734e9b2a01163862cb04
Dave Goehrig <dave@dloh.org>
2016-07-11 15:50:12 -0400

    redoing unframe logic

--- src/websocket_rfc6455.erl
+++ src/websocket_rfc6455.erl
@@ -1,7 +1,7 @@
 -module(websocket_rfc6455).
 -author({ "David J Goehrig", "dave@dloh.org" }).
 -copyright(<<"© 2012,2013,2016 David J. Goehrig"/utf8>>).
--export([ handshake/2, frame/1, frame/2, frame/3, unframe/4, handle/4 ]).
+-export([ handshake/2, frame/1, frame/2, frame/3, unframe/4, handler/2 ]).
 
 frame(Data) when is_binary(Data) ->
 	frame(Data,1).	%% 1 == text
@@ -18,20 +18,6 @@ frame(Data,Opcode,Masked) when is_binary(Data) ->
 		_ -> 
 			framed(Data, Opcode, Len)
 	end.
-
-%% handle take in a message and return back a list 
-handle(Pid,Socket,Message,OldData) ->
-	case Message of
-		{tcp, Socket, NewData} ->
-			unframe(Pid,Socket,OldData,NewData);
-		{tcp_closed, Socket } ->
-			Pid ! { close, Socket },
-			OldData;
-		_ ->
-			io:format("Unexpected message: ~p~n", [ Message ]),
-			OldData
-	end.
-	
 
 %% Calcuate an rfc6455 handshake
 handshake(Headers,_Data) ->
@@ -86,7 +72,7 @@ framed(Data,Opcode,Mask,Len) ->
 	Res.
 
 
-%%  Mask 
+%%  Mask the data as a client
 mask(<<>>,_Mask,_I,Acc) ->
 	binary:list_to_bin(lists:reverse(Acc));
 mask(<<D:8,Data/binary>>,<<M1:8,M2:8,M3:8,M4:8>> = Mask, I, Acc) ->
@@ -101,6 +87,7 @@ mask(<<D:8,Data/binary>>,<<M1:8,M2:8,M3:8,M4:8>> = Mask, I, Acc) ->
 mask(Data,Mask) ->
 	mask(Data,Mask,0,[]).
 
+%% remove a mask form the data from the client
 unmask(<<>>,_Mask,_I,Acc) ->
 	lists:reverse(Acc);
 unmask(<<D:8,Data/binary>>,<<M1:8,M2:8,M3:8,M4:8>> = Mask,I,Acc) ->
@@ -117,85 +104,90 @@ unmask(Data,<<"">>) when is_binary(Data) ->
 unmask(Data,Mask) when is_binary(Data) ->
 	unmask(Data,Mask,0,[]).
 
-%% Peek extracts the header data out of the frame
-peek(<<F:1,_R1:1,_R2:1,_R3:1,Opcode:4,1:1,PayLen:7,Data/binary>>) ->	%% Mask bit set
-	{ length, F, Opcode, masked, PayLen, Data };
-peek(<<F:1,_R1:1,_R2:1,_R3:1,Opcode:4,0:1,PayLen:7,Data/binary>>) ->	%% No Mask bit set
-	{ length, F, Opcode, unmasked, PayLen, Data }.
 
-peek({ length, F, Opcode, masked, 126, <<Length:16,Remainder/binary>> = Data) when byte_size(Data) > 1 ->
-	{ unmask, F, Opcode, masked, Length, Remainder };
-
-peek({ length, F, Opcode, masked, 127, <<Length:64,Remainder/binary>> = Data) when byte_size(Data) > 3 ->
-	{ unmask, F, Opcode, Length, Remainder };
-
-peek({ length, F, Opcode, masked, Length, Data }) ->
-	{ unmask, F, Opcode, masked, Length, Data };
-
-peek({ length, F, Opcode, unmasked, 126, <<Length:16,Remainder/binary>> = Data) when byte_size(Data) > 1 ->
-	{ unpack, F, Opcode, unmasked, Length, Data };
-
-peek({ length, F, Opcode, unmasked, 127, <<Length:64,Remainder/binary>> = Data) when byte_size(Data) > 3 ->
-	{ unpack, F, Opcode, unmasked, Length, Data };
-
-peek({ length, F, Opcode, unmasked, Length, Data }) ->
-	{ unpack, F, Opcode, unmasked, Length, Data };
-
-peek({ unmask, F, Opcode, masked, Length, <<Mask:4/binary,Remainder/binary>> = Data) when byte_size(Data) > 3  ->
-	{ unpack, F, Opcode, Mask, Length, Remainder };
-
-peek({ unpack, F, Opcode, unmasked, Length, <<Payload:Length/binary,Remainder/binary>> = Data }) when byte_size(Data) >= Length ->
-	{ deliver, F, Opcode, Mask, Length, Payload, Remainder };
-
-peek({ unpack, F, Opcode, Mask, Length, <<Payload:Length/binary,Remainder/binary>> = Data }) when byte_size(Data) >= Length ->
-	{ deliver, F, Opcode, Mask, Length, unmask(Payload,Mask), Remainder };
-
-peek(Rec) ->	%% pass through anything we can't process yet
-	Rec.
-
-
-%% unframe 
-unframe(_Pid,_Socket,[],<<>>)  ->						%% when we have no more data, reset socket state
-	[];									%% an empty list indicates there was no previous frame.
-unframe(Pid,Socket,[],NewData) when byte_size(NewData) > 2 ->
-	case peek(NewData) of
-		{ length, 
-	
-	{ Finished, Opcode, Length, Mask, Remainder } = peek(NewData),		%% we check to see if we have a valid frame header
-	RemainderLength = iolist_size(Remainder),
-	if (Length > RemainderLength) ->					%% but if we're short bytes for this header
-		{ Finished, Opcode, Length, Mask, [], Remainder };		%% we save it for next time..
-	(Finished =:= 1) -> 
-		{ Payload, Rest } = decode(Remainder,Length,Mask),		%% If we found we got a complete frame w/ data, we decode it
-		dispatch(Pid,Socket,Opcode,[Payload]),				%% and dispatch to execute the associated method on it
-		unframe(Pid,Socket,[],Rest);					%% then process the remainder as the start of a new frame.
-	%% we have a continuation frame....
-	true ->								 	%% If we're not finished collecting frames but have a full frame
-		{ Payload, Rest } = decode(Remainder,Length,Mask),		%% try to decode and then process the next frame
-		unframe(Pid,Socket,{ Finished, Opcode, Length, Mask, [ Payload ], <<>> }, Rest)
+% wait for data
+unframe(Pid,Socket,{ wait, Data },Payloads) ->
+	receive 
+		NewData when is_binary(NewData) ->
+			unframe(Pid,Socket,{ parse, <<Data/binary,NewData/binary>> },Payloads);
+		Msg ->
+			io:format("unknown message ~p~n", [ Msg ]),
+			unframe(Pid,Socket,wait,Payloads)
 	end;
-unframe(_Pid,_Socket,OldData,<<>>) ->						%% we have no more data, Preserve the previous state...
-	io:format("[unframe] ~p~n", [ OldData ]),
-	OldData;
-unframe(Pid,Socket,OldData,NewData) ->						%% now we've seen some data, but haven't finished processing
-	{ Finished, Opcode, Length, Mask, Payloads, Remainder } = OldData,	%% we may have Payload, Remainder, or Payload&Remainder
-	Rest = <<Remainder/binary,NewData/binary>>,				%% we concatenate the new data onto the leftovers
-	RestLength = iolist_size(Rest),
-	if (Length > RestLength) ->
-		{ Finished, Opcode, Length, Mask, Payloads, Rest };		%% we have more but not enough, save the aggregate for later
-	(Finished =:= 1) ->
-		{ Payload2, Rest2 } = decode(Rest,Length,Mask),			%% here we finished a frame so we decode that frame
-		dispatch(Pid,Socket,Opcode, [ Payload2 | Payloads ]),		%% we conjoin the payload from the previous frames with the new data
-		unframe(Pid,Socket,[],Rest2);					%% start processing the next frame if any exists
-	%% we have a continuation frame....
-	true ->										%% here we have a complete continuation frame so we
-		{ Payload2, Rest2 } = decode(Rest,Length,Mask),				%% decode the current continuation frame and
-		if byte_size(Rest2) > 2 ->
-			{ Finished2, Opcode2, Length2, Mask2, _Remainder2 } = peek(Rest2),	%% attempt to decode the start of the next frame
-			unframe(Pid,Socket, { Finished2, Opcode2, Length2, Mask2, [ Payload2 | Payloads ], <<>> }, Rest2 );
-		true ->
-			io:format("Invalid frame offset"),
-			[]
-		end
+
+unframe(Pid,Socket, { parse, <<>> }, Payloads) ->
+	unframe(Pid,Socket,{ wait, <<>> },Payloads);
+
+% mask bit, and extract Finished bit, Opcode, and PayLen
+unframe(Pid,Socket,{ parse, <<F:1,_R1:1,_R2:1,_R3:1,Opcode:4,1:1,PayLen:7,Data/binary>>}, Payloads) ->
+	unframe(Pid,Socket,{ length, F, Opcode, masked, PayLen, Data }, Payloads);
+
+% unmaked bit and extract Finished bit, Opcode, and PayLen
+unframe(Pid,Socket,{ parse, <<F:1,_R1:1,_R2:1,_R3:1,Opcode:4,0:1,PayLen:7,Data/binary>>}, Payloads) ->
+	unframe(Pid,Socket,{ length, F, Opcode, unmasked, PayLen, Data }, Payloads);
+
+% wait for more data, because we can't parse a frame
+unframe(Pid,Socket,{ parse, Data },Payloads) ->
+	io:format("unframe error ~p~n", [ Data ]),
+	unframe(Pid,Socket, { wait, Data } ,Payloads);
+
+% 16bit length for masked
+unframe(Pid,Socket,{ length, F, Opcode, masked, 126, <<Length:16,Remainder/binary>> = Data }, Payloads) when byte_size(Data) > 1 ->
+	unframe(Pid,Socket,{ unmask, F, Opcode, masked, Length, Remainder }, Payloads);
+
+% 64bit length for masked
+unframe(Pid,Socket,{ length, F, Opcode, masked, 127, <<Length:64,Remainder/binary>> = Data }, Payloads) when byte_size(Data) > 3 ->
+	unframe(Pid,Socket,{ unmask, F, Opcode, Length, Remainder }, Payloads);
+
+% 7bit length for masked
+unframe(Pid,Socket,{ length, F, Opcode, masked, Length, Data }, Payloads) ->
+	unframe(Pid,Socket,{ unmask, F, Opcode, masked, Length, Data }, Payloads);
+
+% 16bit length for unmasked
+unframe(Pid,Socket,{ length, F, Opcode, unmasked, 126, <<Length:16,Remainder/binary>> = Data }, Payloads) when byte_size(Data) > 1 ->
+	unframe(Pid,Socket,{ unpack, F, Opcode, unmasked, Length, Remainder }, Payloads);
+
+% 64bit length for unmasked
+unframe(Pid,Socket,{ length, F, Opcode, unmasked, 127, <<Length:64,Remainder/binary>> = Data }, Payloads) when byte_size(Data) > 3 ->
+	unframe(Pid,Socket,{ unpack, F, Opcode, unmasked, Length, Remainder }, Payloads);
+
+% 7bit length for unmasked
+unframe(Pid,Socket,{ length, F, Opcode, unmasked, Length, Data }, Payloads) ->
+	unframe(Pid,Socket,{ unpack, F, Opcode, unmasked, Length, Data }, Payloads);
+
+% extract 32bit mask
+unframe(Pid,Socket,{ unmask, F, Opcode, masked, Length, <<Mask:4/binary,Remainder/binary>> = Data }, Payloads) when byte_size(Data) > 3  ->
+	unframe(Pid,Socket,{ unpack, F, Opcode, Mask, Length, Remainder }, Payloads);
+
+% payload for unmasked
+unframe(Pid,Socket,{ unpack, F, Opcode, unmasked, Length, Data },Payloads) when byte_size(Data) >= Length ->
+	<<Payload:Length/binary,Remainder/binary>> = Data,
+	unframe(Pid,Socket,{ deliver, F, Opcode, unmasked, Length, Remainder }, [ Payload | Payloads]);
+
+% payload for masked,and decode
+unframe(Pid,Socket,{ unpack, F, Opcode, Mask, Length, Data }, Payloads) when byte_size(Data) >= Length ->
+	<<Payload:Length/binary,Remainder/binary>> = Data,
+	unframe(Pid,Socket,{ deliver, F, Opcode, Mask, Length, Remainder }, [ unmask(Payload,Mask) | Payloads ]);
+
+% continutation frame
+unframe(Pid,Socket,{ deliver, 0, _Opcode, _Mask, _Length, Data }, Payloads) ->
+	unframe(Pid,Socket,{ parse, Data }, Payloads);
+
+% finished frame
+unframe(Pid,Socket,{ deliver, 1, Opcode, _Mask, _Length, Data }, Payloads) ->
+	dispatch(Pid,Socket,Opcode,Payloads),
+	unframe(Pid,Socket,{ parse, Data },[]);
+
+%% wait for new data, so we can advance to the next stage
+unframe(Pid,Socket,{ Stage, F, Opcode, Mask, Length, Data }, Payloads) ->
+	receive 
+		NewData when is_binary(NewData) ->
+			unframe(Pid,Socket, { Stage, F, Opcode, Mask, Length, <<Data/binary,NewData/binary>>}, Payloads);
+		Msg ->
+			io:format("unknown message ~p~n", [ Msg ]),
+			unframe(Pid,Socket,{ Stage, F, Opcode, Mask, Length, Data }, Payloads)
 	end.
-	
+
+%% returns a handler process
+handler(Pid,Socket) ->
+	spawn_link(?MODULE,unframe,[Pid,Socket,{ wait, <<>> },[]]).
